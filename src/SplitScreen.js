e9810364fc94bd6334be799b2a0082cdc18b1f33
Ahmed Elkassas <ahmedelkassas2811@gmail.com>
2023-02-06 18:05:18 +0000

    Add styled-components  & add weight to be more flexiable with flex prop

--- src/SplitScreen.js
+++ src/SplitScreen.js
@@ -1,10 +1,27 @@
-export const SplitScreen = ({ left: Left, right: Right }) => {
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Part = styled.div`
+  flex: ${(props) => props.weight};
+`;
+
+export const SplitScreen = ({
+  left: Left,
+  right: Right,
+  leftWeight,
+  rightWeight,
+}) => {
   return (
-    <div>
-      <Right />
-      <div>
+    <Container>
+      <Part weight={leftWeight}>
         <Left />
-      </div>
-    </div>
+      </Part>
+      <Part weight={rightWeight}>
+        <Right />
+      </Part>
+    </Container>
   );
 };
